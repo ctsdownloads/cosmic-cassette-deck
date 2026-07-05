47af91e7b4001204c2873160a3c955befc8d7bf5
Matt Hartley <matt@matthartley.com>
2026-07-05 14:47:43 -0700

    Set version to 0.0.5

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "cosmic-cassette-deck";
-          version = "0.2.0";
+          version = "0.0.5";
 
           src = ./.;
 
